7b642bb0917dcab2ddbf316f418523135c19b820
EC2 Default User <ec2-user@ip-172-31-26-134.us-east-2.compute.internal>
2018-05-05 05:29:45 +0000

    add favorite model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,8 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true 
   validates :content, presence: true, length: { maximum: 255}
   belongs_to :user
+  
+  has_many :favorites
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'user_id'
+  has_many :favored_users, through: :reverses_of_favorite, source: :user
 end
